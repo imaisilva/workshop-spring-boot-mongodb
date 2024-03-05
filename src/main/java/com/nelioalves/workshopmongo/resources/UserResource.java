e50e938a1d09764374e23f456bda405553404817
Mayara Silva <alohasp@outlook.com>
2024-03-05 18:02:42 -0300

    Atualizacao de usuario com PUT

--- src/main/java/com/nelioalves/workshopmongo/resources/UserResource.java
+++ src/main/java/com/nelioalves/workshopmongo/resources/UserResource.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,7 +38,7 @@ public class UserResource {  	//CONTROLADOR REST
 	}
 	
 	@RequestMapping(method=RequestMethod.POST)
-	public ResponseEntity <Void> insert(UserDTO objDto){
+	public ResponseEntity <Void> insert(@RequestBody UserDTO objDto){
 		User obj = service.fromDTO(objDto);
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("{id}").buildAndExpand(obj.getId()).toUri();
@@ -47,6 +48,14 @@ public class UserResource {  	//CONTROLADOR REST
 	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
 	public ResponseEntity <Void> delete(@PathVariable String id){
 		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	public ResponseEntity <Void> update(@RequestBody UserDTO objDto, @PathVariable String id){
+		User obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 	}
 }
